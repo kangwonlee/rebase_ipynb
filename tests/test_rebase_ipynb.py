f544d4a87e23fd53f466e63aa0de1cc726ff2661
KangWon LEE <kangwon.lee@tukorea.ac.kr>
2023-01-09 22:38:36 +0900

    added eq local file without colab link button

--- tests/test_rebase_ipynb.py
+++ tests/test_rebase_ipynb.py
@@ -33,6 +33,20 @@ def test_verify_processed_ipynb__equal_with_button():
     assert rebase_ipynb.verify_processed_ipynb(src_ipynb_path, dest_ipynb_path)
 
 
+def test_verify_processed_ipynb__equal_without_button():
+    src_ipynb_path = test_folder / 'eq_colab.ipynb'
+    assert src_ipynb_path.exists()
+    assert src_ipynb_path.is_file()
+    assert src_ipynb_path.suffix == '.ipynb'
+
+    dest_ipynb_path = test_folder / 'eq_local_without_button.ipynb'
+    assert dest_ipynb_path.exists()
+    assert dest_ipynb_path.is_file()
+    assert dest_ipynb_path.suffix == '.ipynb'
+
+    assert rebase_ipynb.verify_processed_ipynb(src_ipynb_path, dest_ipynb_path)
+
+
 def test_verify_processed_ipynb__not_equal():
     src_ipynb_path = test_folder / 'ne_colab.ipynb'
     assert src_ipynb_path.exists()
