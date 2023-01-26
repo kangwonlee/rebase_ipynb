5f842ac2fbffdc56bb9ddfb2de0cefda667c4762
KangWon LEE <kangwon.lee@tukorea.ac.kr>
2023-01-26 22:29:12 +0900

    what if only one file changed?

--- tests/test_rebase_ipynb.py
+++ tests/test_rebase_ipynb.py
@@ -98,6 +98,35 @@ def test_get_commit_info_from_show__two_files_changed():
     )
 
 
+def test_get_commit_info_from_show__one_file_changed():
+    git_show_msg = (
+        "commit 717ed6844017766e040d49e6dd562d153aa2bb44\n"
+        "Author: KangWon LEE <kangwon.lee@tukorea.ac.kr>\n"
+        "Date:   Sat Nov 19 09:55:32 2022 +0900\n"
+        "\n"
+        "    init_other_columns()\n"
+        "\n"
+        " sieve.ipynb | 33 ++++++++++++++++++++++++---------\n"
+        " 1 file changed, 24 insertions(+), 9 deletions(-)\n"
+    )
+
+    result = rebase_ipynb.get_commit_info_from_show(git_show_msg)
+
+    assert isinstance(result, dict)
+
+    assert result['author'] == "KangWon LEE"
+    assert result['author_email'] == "kangwon.lee@tukorea.ac.kr"
+    assert result['date'] == "Wed Jan 18 20:57:54 2023 +0900"
+    assert result['message'] == (
+        "checkout_head() -> start_temporary_branch_head()\n"
+        "\n"
+        "also removed start_temporary_branch() to avoid confusion\n"
+        "\n"
+        "for this application, the original intention was\n"
+        "to start the temporary branch at the HEAD of the first commit\n"
+    )
+
+
 def test_start_temporary_branch_head__switch_to_temporary_branch(repo:pathlib.Path):
     new_branch_name = 'rebase_ipynb_test'
 
