67d2cb0b3424654856cfc881c5e96375d68fa6f3
KangWon LEE <kangwon@gmail.com>
2023-01-27 16:34:26 +0900

    parameterized tests

--- tests/test_rebase_ipynb.py
+++ tests/test_rebase_ipynb.py
@@ -5,8 +5,9 @@ import shutil
 import subprocess
 import sys
 import tempfile
+import urllib.parse as up
 
-from typing import Tuple
+from typing import Dict, List, Tuple, Union
 
 import pytest
 
@@ -30,19 +31,132 @@ sys.path.insert(0,
 
 import rebase_ipynb
 
+Commit_log = Union[List[str], Tuple[str]]
+Repo_Info = Dict[str, Union[pathlib.Path, str, Commit_log]]
 
-@pytest.fixture(scope='session')
-def repo() -> pathlib.Path:
+
+def commits_original_sieve() -> Commit_log:
+    return (
+        "86ebb42000d7cdd9af0c320ee24be5dc2fa24a2f",
+        "bae0691b9b6917d951f0161405063f401b058073",
+        "c31c6b24632729a2fadb090ef37d9d879e30c696",
+        "b41002e62d1dfe3ab4143a2f6ba1b5aeeab13e74",
+        "e1c890a2f6dd6a7d6962b9e967527f40b48d89c5",
+        "6a033e9fde2116f24bd62e403a73d69429408a31",
+        "717ed6844017766e040d49e6dd562d153aa2bb44",
+        "74af5773172c4bb7013a44527a688079f1364848",
+        "86eaccc9b8c6bcd275f93fd7ba3d7d15c1b13843",
+        "8e1ebe4b5cc34e86de4be066d93d3f9f6b4c0545",
+        "253bb6edb3d0f1724292384bbab12068d8061133",
+        "9acef1eb750c3e633281b39802511455c56ff0b8",
+    )
+
+
+def commits_original_nmisp() -> Commit_log:
+    return (
+        "7c73e39800254559363a139b282902b42af85cab",
+        "cb9d69afa8885e7fc6411470cd62416ed1dadaa3",
+        "dad8ada84ab13e83d680f3341840582ddef988b7",
+        "379f39d6a217cf3e2f3ff7e28aeea90f3e3df5ea",
+        "d6cab0beb7085678970814dc91617012dcfb41f8",
+        "402a3cacba5c00d7e4fc56c95173701501969e13",
+        "c7fdb8239c83326351e1d51b994c5b2a0f3ea5db",
+        "9939350b317a1e393858c16e770b379c7675242b",
+        "25ecbe9e4ec64a0139a2e10da15048e0d6ed14f7",
+        "787d31e45076f40f583865bb32af077af5fb3a39",
+        "922e8b9b1d2ab2765e125a411d1456579c721402",
+        "facad888eb2606c10f0a20f4a8fc4797b7acb79a",
+        "11edb838d45cb645412a5d8f8c32fe4a1534aa61",
+        "8b2ece1d1eb7f6da4eb7edd051ccf5d75a45974e",
+        "6fd8222acff8a456681ce04955046b85c7fac6d8",
+        "b52e7cf7b7e99a2b83f4e0038ba2027afe8d8cd4",
+        "1d38c296f22d4c9913d2eec77b4cb9ad4a441071",
+        "1ca8df45c34f3776b6878bd2be0dd31581000fd0",
+        "2780538d412bd93baa1536d949d94bc2c5cbae06",
+        "1f429d3d30df13c3b61ba6c0475c23e90f84406a",
+        "15628eb937c8bed2df85b7e9223304b6cd587de2",
+        "baf99312b344bf7619ed9c30fa5c8b9051a25449",
+        "661025a32c2561050ed5920acbe5476ea66e1f1c",
+        "e4ccb17c0f31cb05be7fbd1b39d2dc0ecd38ecfb",
+        "59fe94c9654906960bd1202bd202f1fe5ac6967f",
+        "8078178275d6146968547c1bbe4cf201c9080b7c",
+        "b64a10bfbb41676a06023676418130611084302d",
+        "9fa28b97da0148be1c94fd449cc4bd790705356a",
+        "cd943166841624d99e2ef7ad60d8c071d2f84bc5",
+        "98630891e5e879e6a988fc44908f30fa49db0ddb",
+        "2aed47a285af76bfc2bd8220a094b2d6e0160a93",
+        "139b8b07d9d6c34cc69fd59aee5069f9a4442ce7",
+        "83e099187f9e979245635a70cfcb511a7b6ee2df",
+        "efa5862f650a23e8ad748158e7460f7acbe65587",
+        "35ed39504cf69318d9f23d72b6ca42de1582a3a9",
+        "497a75c1ab6425ca1c3eb6b599624aa0befc151f",
+        "3a0cb8b0eb9b1d7d1e764b9736c9c32efbd7d6fd",
+        "828db88d1ae397aaa752b824a5eb2037bf55e052",
+        "335868d72ecfd19e841894bd24aef94638d91126",
+        "c1ea231fa250a743d86489a34eb1c632a91c62cb",
+        "f319dd7edc840759143731fead9e17af1ebf06fc",
+        "b9de027b795106e248e344b8e8a9c86e343f15ad",
+        "c2219fc6e1eabeb725cac58df9bc960d2ac70bbb",
+        "b327d3c7be207ce27b6cc11f16701691f384acef",
+        "882d9a78d8245ecf6089d3c9996eedb0a334dd15",
+        "54cd0315ed21f9180acc9be84de76e7766710b79",
+        "a607b2be68f113b3a33d683962fe7ffd1e74760e",
+        "3626b33df02f222d4b9a656df07476adcbef5279",
+        "a7f6f455f1470ac39dee5c0be85fcd44b35a9565",
+        "913a1c5538a8035ac8a295b298dbfdf3444ca7dc",
+        "e74d63e6844ce350091e00ff0f61ebbea7bc5150",
+        "b752c683e460228d66fb96ab986cc33cae6f2991",
+        "dd4b283e21a7f3fe77f23cc2a2aa68181950fb3e",
+        "bc13d845555fe6054329edbc914bc012c9323cf1",
+        "031db6e6ae298c6761baa1fe28c489b7d44b487b",
+        "4d4136e02afcd8502e73c8f29ca0dba9e10dfd4d",
+        "98b1e33537f51f8dba402c128866c39f8a74f04e",
+        "390fc239ccabc71f0607dcd6d9603a6a09b482bb",
+        "9b302eb08174112cc679ba978376d747fe9a24d3",
+        "30b00f17dece019652068e5186017ad23e0d3d64",
+        "bdac07abd421ed4fc72adb784a28e9c2c30f0726",
+        "c0b1a033159e08e500a39a77e285e21ef8a2706f",
+        "f25d7ec47cce435a1e6607842d63002eb8c1a4a1",
+        "36b26bc3e1e8c85d7edaa40f6a816c26172d6c11",
+    )
+
+
+@pytest.fixture(
+    scope='session',
+    params=[
+        {
+            'url':'https://github.com/kangwonlee/sieve',
+            'first': '86ebb42',
+            'last': '9acef1e',
+            'commits_original': commits_original_sieve(),
+            'chg': {'sha': "717ed68", "files": ("sieve.ipynb",)}
+        },
+        {
+            'url':'https://github.com/kangwonlee/nmisp',
+            'first': '7c73e398002',
+            'last': '36b26bc3e1e8c',
+            'commits_original': commits_original_nmisp(),
+            'chg': {'sha': "922e8b9", "files": ("00_introduction/20_python_review.ipynb",)}
+        },
+    ]
+)
+def repo_info(request) -> Repo_Info:
     with tempfile.TemporaryDirectory() as tmpdir:
         tmpdir = pathlib.Path(tmpdir)
-        subprocess.run(['git', 'clone', 'https://github.com/kangwonlee/sieve'], cwd=tmpdir)
+        subprocess.run(['git', 'clone', request.param['url']], cwd=tmpdir)
 
-        test_repo_path = tmpdir / 'sieve'
+        test_repo_path = tmpdir / (up.urlparse(request.param['url']).path.split('/')[-1])
 
         test_repo_git_path = test_repo_path / '.git'
         assert test_repo_git_path.exists(), test_repo_path.absolute()
 
-        yield test_repo_path
+        yield {
+            'path': test_repo_path,
+            'first': request.param['first'],
+            'last': request.param['last'],
+            'commits_original': request.param['commits_original'],
+            'chg': request.param['chg'],
+        }
 
 
 def test_get_commit_info_from_show__two_files_changed():
@@ -111,20 +225,22 @@ def test_get_commit_info_from_show__one_file_changed():
     )
 
 
-def test_start_temporary_branch_head__switch_to_temporary_branch(repo:pathlib.Path):
+def test_start_temporary_branch_head__switch_to_temporary_branch(repo_info:Repo_Info):
     new_branch_name = 'rebase_ipynb_test'
 
+    repo_info = repo_info["path"]
+
     # function under test 1
-    rebase_ipynb.start_temporary_branch_head(repo, start_parent="HEAD^", new_branch=new_branch_name)
+    rebase_ipynb.start_temporary_branch_head(repo_info, start_parent="HEAD^", new_branch=new_branch_name)
 
     # list of branch names
-    output = subprocess.check_output(['git', 'branch'], cwd=repo, encoding='utf-8')
+    output = subprocess.check_output(['git', 'branch'], cwd=repo_info, encoding='utf-8')
     output_lines = tuple(map(lambda s:s.strip(), output.splitlines()))
     assert any(map(lambda s:s.endswith(new_branch_name), output_lines)), f"output_lines = {output_lines}"
 
     # function under test 2
-    rebase_ipynb.switch_to_temporary_branch(repo, "main")
-    output = subprocess.check_output(['git', 'branch'], cwd=repo, encoding='utf-8')
+    rebase_ipynb.switch_to_temporary_branch(repo_info, "main")
+    output = subprocess.check_output(['git', 'branch'], cwd=repo_info, encoding='utf-8')
 
     # get current branch name
     output_lines = tuple(map(lambda s:s.strip(), output.splitlines()))
@@ -243,22 +359,11 @@ def test_remove_metadata_id__eq_local():
         assert rebase_ipynb.verify_processed_ipynb(dest_ipynb_path, src_ipynb_path)
 
 
-def test_get_git_log(repo: pathlib.Path):
+def test_get_git_log(repo_info: Repo_Info):
 
-    commits_original = (
-        "86ebb42000d7cdd9af0c320ee24be5dc2fa24a2f",
-        "bae0691b9b6917d951f0161405063f401b058073",
-        "c31c6b24632729a2fadb090ef37d9d879e30c696",
-        "b41002e62d1dfe3ab4143a2f6ba1b5aeeab13e74",
-        "e1c890a2f6dd6a7d6962b9e967527f40b48d89c5",
-        "6a033e9fde2116f24bd62e403a73d69429408a31",
-        "717ed6844017766e040d49e6dd562d153aa2bb44",
-        "74af5773172c4bb7013a44527a688079f1364848",
-        "86eaccc9b8c6bcd275f93fd7ba3d7d15c1b13843",
-        "8e1ebe4b5cc34e86de4be066d93d3f9f6b4c0545",
-        "253bb6edb3d0f1724292384bbab12068d8061133",
-        "9acef1eb750c3e633281b39802511455c56ff0b8",
-    )
+    repo = repo_info["path"]
+
+    commits_original = repo_info["commits_original"]
 
     start_parent = commits_original[0]
     end = commits_original[-1]
@@ -273,14 +378,15 @@ def test_get_git_log(repo: pathlib.Path):
     )
 
 
-def test_get_changed_files(repo: pathlib.Path):
-    result = rebase_ipynb.git_diff_fnames(repo, "717ed68")
+def test_get_changed_files(repo_info: Repo_Info):
+
+    repo = repo_info["path"]
+
+    result = rebase_ipynb.git_diff_fnames(repo, repo_info["chg"]["sha"])
 
     assert isinstance(result, tuple)
 
-    assert set(result) == set([
-        "sieve.ipynb"
-    ])
+    assert set(result) == set(repo_info["chg"]["files"])
 
 
 def test_get_hash_log_cmd():
@@ -297,16 +403,22 @@ def test_get_hash_log_cmd():
     assert result == expected
 
 
-def test_git_parent_sha(repo):
-    result = rebase_ipynb.git_parent_sha(repo, 'bae0691b')
+def test_git_parent_sha(repo_info:Repo_Info):
+    repo = repo_info["path"]
 
-    assert result.startswith("86ebb42")
+    commits_original = repo_info["commits_original"]
+
+    result = rebase_ipynb.git_parent_sha(repo, commits_original[-1])
+
+    assert result.startswith(commits_original[-2])
 
 
-def test_process_commits(repo:pathlib.Path):
+def test_process_commits(repo_info:Repo_Info):
 
-    first_commit = '86ebb42'
-    last_commit = '9acef1e'
+    repo = repo_info["path"]
+
+    first_commit = repo_info["first"]
+    last_commit = repo_info["last"]
 
     # get the commit before the first commit
     start_parent = rebase_ipynb.git_parent_sha(repo, first_commit)
